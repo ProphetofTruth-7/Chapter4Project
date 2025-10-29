da9d4993295bcff89c0cc369da85f23a1fc779ff
Malakye Brown <prophetoftruth07@gmail.com>
2025-10-29 18:32:27 -0400

    completiton

--- Chapter4Project.cpp
+++ Chapter4Project.cpp
@@ -1,9 +1,38 @@
-// Main Branch
+// Shipping Subproject Branch
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    cout << "This is the Shipping Subproject" << endl;
+    double smallRate = 1.10/500;
+    double mediumRate = 2.20/500;
+    double largeRate = 3.70/500;
+    double hugeRate = 4.80/500;
+    double packageSize, costCalc;
+    int travel;
+
+    do {
+        cout << "Enter the size of your package(kg, 0.1-20): " << endl;
+        cin >> packageSize;
+    } while (packageSize >= 21 || packageSize <= 0);
+
+    do {
+        cout << "Enter the distance your package is traveling(mi, 0-3000): " << endl;
+        cin >> travel;
+    } while (travel >= 3001 || travel <= 0);
+
+    if (packageSize > 10) {
+        costCalc = hugeRate;
+    } else if (packageSize <= 10 && packageSize > 6) {
+        costCalc = largeRate;
+    } else if (packageSize <= 6 && packageSize > 2) {
+        costCalc = mediumRate;
+    } else {
+        costCalc = smallRate;
+    }
+
+    cout << "To travel " << travel << " miles with a package weighing " << packageSize << " kilograms, you'll need to pay $" << costCalc * travel << endl;
+
+ return 0;
 }
