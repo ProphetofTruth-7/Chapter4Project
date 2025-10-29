635b5a96c6b2cb4ba1c118f6ecfab0fb8ce6d53a
Malakye Brown <prophetoftruth07@gmail.com>
2025-10-29 18:45:51 -0400

    Checkout for Loop help

--- Chapter4Project.cpp
+++ Chapter4Project.cpp
@@ -1,9 +1,12 @@
-// Main Branch
+// Geometry Calculator Subproject
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    cout << "This is the GeometryCalc Subproject" << endl;
+    int calcChoice;
+
+    cout << "Geometry Calculator\n" << "1. Calculate the Area of a Circle\n" << "2. Calculate the Area of a Rectangle\n" << "3. Calculate the Area of a Triangle\n" << "4. Quit\n" << endl;
+    cin >> calcChoice;
 }
